e0cde8f24f4816fb4128778f985cf3441268f9b4
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 00:13:21 +0100

    Intitial commit. Project created.

---
+++ src/main/java/com/nst/domaci/NST/NstApplication.java
@@ -0,0 +1,13 @@
+package com.nst.domaci.NST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nst/domaci/NST/NstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nst.domaci.NST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
