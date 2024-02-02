cff80664ad23cec64afb8f7ee87f727732a54447
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 01:30:08 +0100

    Introduced DepartmentMismatchException.java to handle specific scenario.

---
+++ src/main/java/com/nst/domaci/NST/exception/DepartmentMismatchException.java
@@ -0,0 +1,7 @@
+package com.nst.domaci.NST.exception;
+
+public class DepartmentMismatchException extends RuntimeException{
+    public DepartmentMismatchException(String message) {
+        super(message);
+    }
+}
