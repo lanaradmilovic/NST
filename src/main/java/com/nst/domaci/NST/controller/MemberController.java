17b951c8c6f8424fe6852094f437c3cb5e60ccd6
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 01:35:23 +0100

    Introduced MemberController.java.

---
+++ src/main/java/com/nst/domaci/NST/controller/MemberController.java
@@ -0,0 +1,48 @@
+package com.nst.domaci.NST.controller;
+
+import com.nst.domaci.NST.dto.MemberDto;
+import com.nst.domaci.NST.service.impl.MemberServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MemberController {
+    private final MemberServiceImpl memberService;
+
+    public MemberController(MemberServiceImpl memberService) {
+        this.memberService = memberService;
+    }
+    @GetMapping("/members")
+    public ResponseEntity<List<MemberDto>> findAll(){
+        return new ResponseEntity<>(memberService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/members/{id}")
+    public ResponseEntity<MemberDto> findById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(memberService.findById(id), HttpStatus.OK);
+    }
+    @GetMapping("/departments/{id}/members")
+    public ResponseEntity<List<MemberDto>> findAllByDepartmentId(@PathVariable("id") Long departmentId){
+        return new ResponseEntity<>(memberService.findAllByDepartmentId(departmentId), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/members/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id){
+        memberService.delete(id);
+        return new ResponseEntity<>("Member removed.", HttpStatus.OK);
+    }
+    @PostMapping("/members")
+    public ResponseEntity<MemberDto> save(@Valid @RequestBody MemberDto MemberDto){
+        return new ResponseEntity<>(memberService.save(MemberDto), HttpStatus.OK);
+    }
+    @PutMapping("/members")
+    public ResponseEntity<MemberDto> update(@Valid @RequestBody MemberDto MemberDto){
+        return new ResponseEntity<>(memberService.update(MemberDto), HttpStatus.OK);
+    }
+    
+}
