fb9eb677935ce8aeca65068b755579fac0276a27
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 01:22:04 +0100

    Introduced MemberNotInDepartmentException.java and RoleConflictException.java to handle specific scenarios.

---
+++ src/main/java/com/nst/domaci/NST/exception/MemberNotInDepartmentException.java
@@ -0,0 +1,8 @@
+package com.nst.domaci.NST.exception;
+
+public class MemberNotInDepartmentException extends RuntimeException {
+    public MemberNotInDepartmentException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/main/java/com/nst/domaci/NST/exception/RoleConflictException.java
@@ -0,0 +1,7 @@
+package com.nst.domaci.NST.exception;
+
+public class RoleConflictException extends RuntimeException{
+    public RoleConflictException(String message) {
+        super(message);
+    }
+}
