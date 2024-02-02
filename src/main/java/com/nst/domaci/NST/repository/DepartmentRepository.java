1326f58b56341641a7c5804d2b7a275470c36cf1
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 01:06:35 +0100

    Introduced DepartmentRepository.java.

---
+++ src/main/java/com/nst/domaci/NST/repository/DepartmentRepository.java
@@ -0,0 +1,13 @@
+package com.nst.domaci.NST.repository;
+
+import com.nst.domaci.NST.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Optional<Department> findByName(String name);
+
+}
