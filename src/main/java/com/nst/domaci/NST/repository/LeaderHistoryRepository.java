b79bb504224619082b876c1721a2e5f32301e26b
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 01:15:26 +0100

    Introduced LeaderHistoryRepository.java.

---
+++ src/main/java/com/nst/domaci/NST/repository/LeaderHistoryRepository.java
@@ -0,0 +1,34 @@
+package com.nst.domaci.NST.repository;
+
+import com.nst.domaci.NST.entity.LeaderHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LeaderHistoryRepository extends JpaRepository<LeaderHistory, Long> {
+    List<LeaderHistory> findAllByDepartmentId(Long departmentId);
+
+    List<LeaderHistory> findAllByMemberId(Long memberId);
+
+    @Query("SELECT lh FROM LeaderHistory lh " +
+            "WHERE lh.department.id = :departmentId " +
+            "AND lh.member.id = :memberId " +
+            "AND lh.endDate IS NULL")
+    List<LeaderHistory> findAllByDepartmentAndMember(@Param("departmentId") Long departmentId,
+                                                     @Param("memberId") Long memberId);
+
+    @Query("SELECT lh FROM LeaderHistory lh " +
+            "WHERE lh.member.id = :memberId " +
+            "AND lh.endDate IS NULL")
+    LeaderHistory findCurrentByMemberId(@Param("memberId") Long memberId);
+
+    @Query("SELECT lh FROM LeaderHistory lh " +
+            "WHERE lh.department.id = :departmentId " +
+            "AND lh.endDate IS NULL")
+    LeaderHistory findCurrentLeaderByDepartmentId(@Param("departmentId") Long departmentId);
+
+}
