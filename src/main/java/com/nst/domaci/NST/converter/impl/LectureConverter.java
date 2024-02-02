0d2b97d2e33c59f1ba0332657205afe5ee618f5e
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 13:53:45 +0100

    fixup! Introduced LectureDto.java and bi-directional dto enity converter LectureConverter.java.

--- src/main/java/com/nst/domaci/NST/converter/impl/LectureConverter.java
+++ src/main/java/com/nst/domaci/NST/converter/impl/LectureConverter.java
@@ -4,7 +4,7 @@ import com.nst.domaci.NST.converter.DtoEntityConverter;
 import com.nst.domaci.NST.dto.LectureDto;
 import com.nst.domaci.NST.entity.Lecture;
 
-public class LectureDtoConverter implements DtoEntityConverter<LectureDto, Lecture> {
+public class LectureConverter implements DtoEntityConverter<LectureDto, Lecture> {
 
     @Override
     public LectureDto toDto(Lecture lecture) {
