26eb9506fdc493862d68cca3665a0cba240c058d
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 01:34:16 +0100

    Introduced AcademicTitleController.java.

---
+++ src/main/java/com/nst/domaci/NST/controller/AcademicTitleController.java
@@ -0,0 +1,44 @@
+package com.nst.domaci.NST.controller;
+
+import com.nst.domaci.NST.dto.AcademicTitleDto;
+import com.nst.domaci.NST.service.impl.AcademicTitleServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/academic-titles")
+public class AcademicTitleController {
+    private final AcademicTitleServiceImpl academicTitleService;
+
+
+    public AcademicTitleController(AcademicTitleServiceImpl academicTitleService) {
+        this.academicTitleService = academicTitleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AcademicTitleDto>> findAll(){
+        return new ResponseEntity<>(academicTitleService.findAll(), HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<AcademicTitleDto> findById(@PathVariable("id") Long id){
+        return new ResponseEntity<>(academicTitleService.findById(id), HttpStatus.OK);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable("id") Long id){
+        academicTitleService.delete(id);
+        return new ResponseEntity<>("Academic title removed.", HttpStatus.OK);
+    }
+    @PostMapping
+    public ResponseEntity<AcademicTitleDto> save(@Valid @RequestBody AcademicTitleDto academicTitleDto){
+        return new ResponseEntity<>(academicTitleService.save(academicTitleDto), HttpStatus.OK);
+    }
+    @PutMapping
+    public ResponseEntity<AcademicTitleDto> update(@Valid @RequestBody AcademicTitleDto academicTitleDto){
+        return new ResponseEntity<>(academicTitleService.update(academicTitleDto), HttpStatus.OK);
+    }
+
+}
