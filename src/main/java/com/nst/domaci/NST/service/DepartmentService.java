fe7cf474d377f35ed1fd7b2b3d8dd00a84c24f69
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-02 01:07:07 +0100

    Introduced DepartmentService.java.

---
+++ src/main/java/com/nst/domaci/NST/service/DepartmentService.java
@@ -0,0 +1,22 @@
+package com.nst.domaci.NST.service;
+
+import com.nst.domaci.NST.dto.DepartmentDto;
+import com.nst.domaci.NST.exception.EntityAlreadyExistsException;
+import com.nst.domaci.NST.exception.ResourceNotFoundException;
+
+import java.util.List;
+
+public interface DepartmentService {
+    List<DepartmentDto> findAll();
+
+    DepartmentDto findById(Long id) throws ResourceNotFoundException;
+
+    DepartmentDto save(DepartmentDto departmentDto) throws EntityAlreadyExistsException;
+
+    DepartmentDto update(DepartmentDto departmentDto) throws ResourceNotFoundException;
+
+    void delete(Long id) throws ResourceNotFoundException;
+    void setLeader(Long departmentId, Long memberId);
+    void setSecretary(Long departmentId, Long memberId);
+
+}
