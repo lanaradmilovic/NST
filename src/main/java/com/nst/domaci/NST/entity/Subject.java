d9102a777c0c8f3699116f39f33d5f045ab6c145
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-02-09 02:12:29 +0100

    Changed type of espb from String to int.

--- src/main/java/com/nst/domaci/NST/entity/Subject.java
+++ src/main/java/com/nst/domaci/NST/entity/Subject.java
@@ -1,11 +1,15 @@
 package com.nst.domaci.NST.entity;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.nst.domaci.NST.serialization.SubjectSerializer;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.time.Year;
 import java.util.List;
+import java.util.Optional;
 
 @Data
 @NoArgsConstructor
@@ -14,6 +18,7 @@ import java.util.List;
 @Table
 @Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@JsonSerialize(using = SubjectSerializer.class)
 public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,12 +26,26 @@ public class Subject {
     @NotEmpty(message = "Subject name is mandatory")
     @Size(min = 2, max = 50, message = "Subject name is between 2 and 50 characters long")
     private String name;
-    private String espb;
+    private int espb;
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "department_id")
     private Department department;
     @OneToMany(mappedBy = "subject", fetch = FetchType.LAZY)
     private List<Engagement> engagements;
+    @OneToOne(fetch = FetchType.EAGER, mappedBy = "subject", cascade = CascadeType.ALL)
+    private Fund fund;
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "subject", cascade = CascadeType.ALL)
+    private List<LectureSchedule> lectureSchedules;
+
+    public LectureSchedule getLectureScheduleCurrentYear() {
+        int currentYear = Year.now().getValue();
+
+        Optional<LectureSchedule> currentYearLectureSchedule = lectureSchedules.stream()
+                .filter(schedule -> schedule.getYear() == currentYear)
+                .findFirst();
+
+        return currentYearLectureSchedule.orElse(null);
+    }
 
 
 }
--- src/main/resources/liquibase/setup/ddl/create-subject.sql
+++ src/main/resources/liquibase/setup/ddl/create-subject.sql
@@ -1,7 +1,7 @@
 CREATE TABLE subject (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(10) NOT NULL CHECK (LENGTH(name) BETWEEN 2 AND 10),
-    espb VARCHAR(255),
+    espb BIGINT,
     department_id BIGINT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
