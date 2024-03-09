5e964f322270497d70dd4ac19a667dd3b89dd4d5
Lana Radmilovic <lr20190131@student.fon.bg.ac.rs>
2024-03-09 16:11:21 +0100

    Initialization of subject table done.

--- src/main/resources/liquibase/setup/ddl/create-subject.sql
+++ src/main/resources/liquibase/setup/ddl/create-subject.sql
@@ -1,6 +1,6 @@
 CREATE TABLE subject (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(10) NOT NULL CHECK (LENGTH(name) BETWEEN 2 AND 10),
+    name VARCHAR(50) NOT NULL CHECK (LENGTH(name) BETWEEN 2 AND 50),
     espb BIGINT,
     department_id BIGINT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id)
---
+++ src/main/resources/liquibase/setup/dml/init-subject.sql
@@ -0,0 +1,11 @@
+DELETE FROM subject;
+
+INSERT INTO subject (name, espb, department_id)
+VALUES
+    ('Software Engineering Principles', 6, 1),
+    ('Programming 1', 6, 1),
+    ('Programming 2', 6, 1),
+    ('Quality Management Systems', 5, 2),
+    ('Information Systems Design', 7, 3),
+    ('Electronic Business Strategies', 6, 4),
+    ('Operations Research Techniques', 8, 5);
